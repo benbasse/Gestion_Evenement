cb30a825e220b28e4476f8e0b879ce48623c4dfd
benbasse <moussabasse221@gmail.com>
2023-11-28 19:50:46 +0000

    Model Client

--- Events/app/Models/Client.php
+++ Events/app/Models/Client.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Foundation\Auth\Client as Authenticatable;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Client extends Model
+class Client extends Authenticatable
 {
     use HasFactory;
+    
 }
